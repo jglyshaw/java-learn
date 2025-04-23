43b530fedd1869784da9b37e71ca82fde52e39f0
Josiah Glyshaw <josiah@glyshaw.com>
2025-04-23 17:43:12 -0500

    add springboot project

---
+++ my-store/src/test/java/com/jglyshaw/my_store/MyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jglyshaw.my_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
